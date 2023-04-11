0764d6dbddf25c6fc2240bb0567eae646da07cbc
Steve Occhipinti <dev@stevenocchipinti.com>
2023-04-11 20:30:14 +1000

    Move the meta tags into _app
    There were console warnings having it in _document and a link to an article for how to fix it:
    https://nextjs.org/docs/messages/no-document-viewport-meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app"
+import Head from "next/head"
 import { globalCss, createTheme, NextUIProvider } from "@nextui-org/react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
@@ -35,6 +36,19 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       }}
     >
       <NextUIProvider>
+        <Head>
+          <title>Timer</title>
+          <link rel="icon" href="/favicon.ico" />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+          <meta name="apple-mobile-web-app-title" content="Timer" />
+          <meta name="application-name" content="Timer" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#000000" />
+        </Head>
         <Component {...pageProps} />
       </NextUIProvider>
     </NextThemesProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,20 +26,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {CssBaseline.flush()}
-          <title>Timer</title>
-          <link rel="icon" href="/favicon.ico" />
-
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-          <meta name="apple-mobile-web-app-title" content="Timer" />
-          <meta name="application-name" content="Timer" />
-          <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="theme-color" content="#000000" />
-        </Head>
+        <Head>{CssBaseline.flush()}</Head>
         <body>
           <Main />
           <NextScript />
