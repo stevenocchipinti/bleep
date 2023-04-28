c58fc04c254280649be6208e0d425dd162cbd754
Steve Occhipinti <dev@stevenocchipinti.com>
2023-04-28 20:04:36 +1000

    Add NextPWA for a basic service worker
    This probably doesn't do too much just yet but will hopefully make this app work
    offline eventually.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,23 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Timer</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+
+        <meta name="application-name" content="Bleep" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Bleep" />
+        <meta name="description" content="An exercise timer that goes bleep!" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
