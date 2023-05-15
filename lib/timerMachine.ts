2ae31a558187811ca26ba62ee44076f8f54f18bf
Steve Occhipinti <dev@stevenocchipinti.com>
2023-05-15 23:08:52 +1000

    Put logic into gaurds and actions instead of a function

--- lib/timerMachine.ts
+++ lib/timerMachine.ts
@@ -10,7 +10,7 @@ type LoadedEvent = {
 
 const timerMachine = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5QBcCWBbMAnAdAGQHsBDCVAOygGIICywdyA3Aga3rU10JPKgSYIBjImloBtAAwBdSVMSgADgVipRZeSAAeiACwBWHHr0SAjAA4JOgGx6zJgEw6AzABoQAT0QBaExKs5LAE4TAHZHKydAvStHAF9Ytw5sHFhkAgUFSEoAZQAVAEEAJVzZDSUVNQ1tBCcJQJwdCRDm+xCzPXtg1w9EBwNAgacQyNsQ50t4xIxkrABXMjJeSgAFfIBVbIBRUqQQctVUWirEeybDYesnJ2MJJxs9N08EHU6cK2d2wJ0zYaszK0mICSuDmCyWhU2WxK0jKygOR121ScrUMPx+VjGgT8Jh0j0QYRwZheERaWPsNgBCSB0xB80WFEoO0UcMqiN0fwaUXsTmcRnadjxz3s5yiVluL3s0TMgOBOFB9KgOEEBHmaAoAAIaAB3MiUXIASQAwgBpJl7FmHdRs552AJtYI6LF6a5-QXOkKEwJmMxOEzI8l-ewymk4BREWawLIQqFm-as0DVZ04AM895EmwmJ2CyX1cZY0IOsXNYOcUPhyMQHIFYqxi0IhMnM7DELO-32VpGXE9GpWeqZv2mVqBewOHTxKlkAgQOAaYGwiqW44ILynHAF50-ExGEeRQVeZEGEJWDNfIa1MxfEvJbikCjz+FWhsIFFmU7WBxEnRtKwmPfcszJseHT6HUXqBO8V64Kk6SZBA97xloiDXG81jvCOFi9g4gSCiY-ivlExg2F+uFjJBcp0rw8GLtaQx9t6-oSB0YqtFYgpXP4GY4nogS+hS-xkfKvBKiqZBqlAmoEDqVH1ohQoeuYPrtox5ISCxgovDoa56CYOISPygRHkGVKymGEaQNJj6yR0yatK0Ol3MEF7qa87xON6GJfAMrTjrEQA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QBcCWBbMAnAdAGQHsBDCVAOygGIICywdyA3Aga3rU10JPKgSYIBjImloBtAAwBdSVMSgADgVipRZeSAAeiACwBWHHr0SAjAA4JOgGx6zJgEw6AzABoQAT0QBaExKs5LAE4TAHZHKydAvStHAF9Ytw5sHFhkAgUFSEoAZQAVAEEAJVzZDSUVNQ1tBCcJQJwdCRDm+xCzPXtg1w9EBwNAgacQyNsQ50t4xIxkrABXMjJeSgAFfIBVbIBRUqQQctVUWirEeybDYesnJ2MJJxs9N08EHU6cK2d2wJ0zYaszK0mICSuDmCyWhU2WxK0jKygOR121ScrUMPx+VjGgT8Jh0j0QYRwZheERaWPsNgBCSB0xB80WFEoO0UcMqiN0fwaUXsTmcRnadjxz3s5yiVluL3s0TMgOBOFB9KgOEEBHmaAoAAIaAB3MiUXIASQAwgBpJl7FmHdRs552AJtYI6LF6a5-QXOkKEwJmMxOEzI8l-ewymk4BREWawLIQqFm-as0DVZ04AM895EmwmJ2CyX1cZY0IOsXNYOcUPhyMQHIFYqxi0IhMnM7DELO-32VpGXE9GpWeqZv2mVqBewOHQlmZ03hKlVkNVQTUEHWMmG7OOW44IKy9t46ZohYLktqOwX6Mw4HlmRzmfcDKwmeJUsgECBwDTA2EVdfWrynHAF50-CYRgjpEgpeMiBghFuegmDYjRRM645cMQpAUB+8JWg2CAopelh3vYRK7v8JhgdyZ4Bh0+h1F6gTvEhKRpBkkDofGWiINcO5bi85h+AegSCrBhKdEYfh6LusFjPR8q8CxX5YUMfbev6EgdGKrRWIKVz+BmOLegRpgqXoUmThQ06qrwC46rJ9ZsUKHrmD67YqeSEjqSejh-jBOISPygRQUGVKymGEbMaudaYbZHTJq0rQmH6vbmPx3YvPU7xON6GJfAMrQPrEQA */
     id: "timer",
     initial: "Loading",
     context: {
@@ -69,11 +69,25 @@ const timerMachine = createMachine(
             },
 
             on: {
-              TICK: {
-                target: "counting down",
-                internal: true,
-                actions: "decrementTimer",
-              },
+              TICK: [
+                {
+                  target: "counting down",
+                  internal: true,
+                  actions: "decrementTimer",
+                },
+                {
+                  target: "counting down",
+                  internal: true,
+                  actions: "incrementBlock",
+                },
+              ],
+            },
+
+            always: {
+              target: "counting down",
+              internal: true,
+              cond: "blockFinished",
+              actions: "incrementBlock",
             },
           },
         },
@@ -101,28 +115,20 @@ const timerMachine = createMachine(
         !!program &&
         secondsRemaining === 0 &&
         currentBlockIndex === program.blocks.length - 1,
+      blockFinished: ({ program, currentBlockIndex, secondsRemaining }) =>
+        !!program &&
+        secondsRemaining === 0 &&
+        currentBlockIndex < program.blocks.length - 1,
     },
     actions: {
-      decrementTimer: assign(context => {
-        const { program, currentBlockIndex, secondsRemaining } = context
-        if (!program || currentBlockIndex === null) return context
+      decrementTimer: assign({
+        secondsRemaining: ({ secondsRemaining }) => secondsRemaining - 1,
+      }),
 
-        let newSecondsRemaining = secondsRemaining - 1
-        let newCurrentBlockIndex = currentBlockIndex
-
-        if (
-          newSecondsRemaining <= 0 &&
-          currentBlockIndex < program.blocks.length - 1
-        ) {
-          newCurrentBlockIndex++
-          newSecondsRemaining = program.blocks[newCurrentBlockIndex].seconds
-        }
-
-        return {
-          secondsRemaining: newSecondsRemaining,
-          currentBlockIndex: newCurrentBlockIndex,
-          program,
-        }
+      incrementBlock: assign({
+        currentBlockIndex: ({ currentBlockIndex }) => currentBlockIndex + 1,
+        secondsRemaining: ({ program, currentBlockIndex }) =>
+          program?.blocks[currentBlockIndex + 1].seconds || 0,
       }),
 
       resetTimer: assign({
@@ -137,15 +143,12 @@ const timerMachine = createMachine(
     },
     services: {
       startTimer: () => send => {
-        send("TICK")
         const interval = setInterval(() => send("TICK"), 1000)
         return () => clearInterval(interval)
       },
       loadData: () => {
         return new Promise(resolve => {
-          setTimeout(() => {
-            resolve(dummyProgram)
-          }, 1000)
+          setTimeout(() => resolve(dummyProgram), 1000)
         })
       },
     },
