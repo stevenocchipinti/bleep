afa3ed051b338cd8002c4928f0564e1f936aada9
anishali2 <ansihali1221@gmail.com>
2024-01-17 23:45:06 +0500

    fix the smooth drag and drop issue

--- components/CardButton.tsx
+++ components/CardButton.tsx
@@ -60,76 +60,76 @@ const CardButton = ({
   }
 
   return (
-    <Card
-      transition="0.2s"
-      transform={selected ? "scale(1.05)" : undefined}
-      opacity={disabled ? 0.7 : 1}
-      variant={selected ? "filled" : undefined}
-      bg={selected ? "gray.600" : undefined}
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...props}
-    >
-      <CardHeader onClick={onClick} display="flex" alignItems="center" p={0}>
-        <Flex
-          justifyContent="center"
-          alignItems="center"
-          h="full"
-          px={2}
-          {...listeners}
-        >
-          <DragHandleIcon color={selected ? "gray.500" : "gray.600"} />
-        </Flex>
+    <div style={style} {...attributes}>
+      <Card
+        transition="0.2s"
+        transform={selected ? "scale(1.05)" : undefined}
+        opacity={disabled ? 0.7 : 1}
+        variant={selected ? "filled" : undefined}
+        bg={selected ? "gray.600" : undefined}
+        ref={setNodeRef}
+        {...props}
+      >
+        <CardHeader onClick={onClick} display="flex" alignItems="center" p={0}>
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            h="full"
+            px={2}
+            {...listeners}
+          >
+            <DragHandleIcon color={selected ? "gray.500" : "gray.600"} />
+          </Flex>
 
-        {error === true ? (
-          <ErrorChip />
-        ) : (
-          <>
-            {message === true && <MessageChip disabled={disabled} />}
-            {typeof seconds === "number" && (
-              <TimerChip disabled={disabled} seconds={seconds} />
-            )}
-            {typeof reps === "number" && (
-              <PauseChip disabled={disabled} reps={reps} />
-            )}
-          </>
-        )}
+          {error === true ? (
+            <ErrorChip />
+          ) : (
+            <>
+              {message === true && <MessageChip disabled={disabled} />}
+              {typeof seconds === "number" && (
+                <TimerChip disabled={disabled} seconds={seconds} />
+              )}
+              {typeof reps === "number" && (
+                <PauseChip disabled={disabled} reps={reps} />
+              )}
+            </>
+          )}
 
-        <Text my={3} fontSize="lg" fontWeight={selected ? "bold" : "normal"}>
-          {text}
-        </Text>
+          <Text my={3} fontSize="lg" fontWeight={selected ? "bold" : "normal"}>
+            {text}
+          </Text>
 
-        <Spacer />
+          <Spacer />
 
-        <IconButton
-          display="flex"
-          variant="ghost"
-          aria-label="Toggle body"
-          m={1}
-          onClick={
-            typeof isExpanded === "boolean" ? undefined : innerButtonOnClick
-          }
-          icon={
-            <ChevronRightIcon
-              transition="0.2s"
-              transform={
-                typeof isExpanded === "boolean"
-                  ? transforms[isExpanded ? "up" : "down"]
-                  : undefined
-              }
-              boxSize={5}
-            />
-          }
-        />
-      </CardHeader>
+          <IconButton
+            display="flex"
+            variant="ghost"
+            aria-label="Toggle body"
+            m={1}
+            onClick={
+              typeof isExpanded === "boolean" ? undefined : innerButtonOnClick
+            }
+            icon={
+              <ChevronRightIcon
+                transition="0.2s"
+                transform={
+                  typeof isExpanded === "boolean"
+                    ? transforms[isExpanded ? "up" : "down"]
+                    : undefined
+                }
+                boxSize={5}
+              />
+            }
+          />
+        </CardHeader>
 
-      <Collapse in={isExpanded} animateOpacity>
-        <CardBody p={4} pt={0}>
-          {children}
-        </CardBody>
-      </Collapse>
-    </Card>
+        <Collapse in={isExpanded} animateOpacity>
+          <CardBody p={4} pt={0}>
+            {children}
+          </CardBody>
+        </Collapse>
+      </Card>
+    </div>
   )
 }
 
